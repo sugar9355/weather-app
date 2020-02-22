01f671d29f3213288cf3301d19fa29c7d0042483
raza9355 <58571587+raza9355@users.noreply.github.com>
2020-02-22 18:28:02 +0500

    add

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,7 @@ weatherForm.addEventListener("submit", e => {
   if (location.length === null) {
     console.log(data.error);
   } else {
-    fetch("http://localhost:3000/weather?address=" + location + "").then(
+    fetch('/weather?address=' + location + "").then(
       response => {
         response.json().then(data => {
           if (data.error) {
